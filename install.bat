1d27e61f568a9b9137e3da5566e438de6b895227
YeoheungMinSsi <165755800+YeoheungMinSsi@users.noreply.github.com>
2024-07-05 07:05:36 +0900

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Installing required packages...
-pip install mediapipe opencv-python playsound gtts pillow numpy pandas
+pip install mediapipe opencv-python playsound gtts pillow numpy pandas django
 echo Installation complete!
 pause
